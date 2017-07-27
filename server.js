f9f97831691bb77a63197bfc0cf7684b5cc03600
Anders Wood <anderswood@gmail.com>
2017-07-27 10:42:46 -0600

    update port to be dynaic

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.set('port', 3001);
+app.set('port', process.env.PORT || 3001);
 
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.json());
